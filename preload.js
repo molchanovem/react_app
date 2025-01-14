a01dd9eba0463e6e265c287bed50548e3ed16ae8
molchanovem <molcanovm418@gmail.com>
2025-01-14 16:55:00 +0300

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  loadEmployees: () => ipcRenderer.invoke('load-employees'),
-  addEmployee: (employee) => ipcRenderer.invoke('add-employee', employee),
-  editEmployee: (employee) => ipcRenderer.invoke('edit-employee', employee),
-  deleteEmployee: (id) => ipcRenderer.invoke('delete-employee', id),
-});
